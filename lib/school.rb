afc39cc429ffd294ae3d41f10fefecce47ce2515
Cydnee Owens <cks410@yahoo.com>
2020-08-19 14:26:10 -0600

    Add iteration 2/3

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,10 @@
 class School
-  attr_reader :start_time, :hours
+  attr_reader :start_time, :hours, :student_names, :time
   def initialize(start_time, hours)
     @start_time = start_time
     @hours = hours
+    @student_names = []
+    @time = "#{@time}:00"
   end
 
   def hours_in_school_day
@@ -10,7 +12,20 @@ class School
   end
 
   def student_names
-    student_names = []
+    @student_names
+  end
+
+  def add_student_name(name)
+    @student_names.push(name)
+  end
+
+  # def end_time
+  #   @time = @hours + @start_time
+  # end
+
+  def is_full_time?
+    if @hours > 4 == true
+    end
   end
 
 end
